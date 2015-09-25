1186227d49d496f96792b8f078e0731c8e2302f9
Mitch Almon Brooks <mbrooks2@silk3.uvm.edu>
2015-09-25 13:48:57 -0400

    message about what you did

---
+++ greetings.php
@@ -0,0 +1,27 @@
+<?php
+function Greetings($hours = 0){
+        
+        
+        print"<p>A function".$hours
+        $hours = (int).$hours
+if ($hours<'11'){
+echo 'Good Morning';}
+    elseif ($hours<4 ){
+    echo 'Good Afternoon';}
+    elseif ($hours<9) {
+    echo 'Good Evening';
+    }
+    else {
+        echo 'Good Night';
+    
+}
+    
+}
+    
+    
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
